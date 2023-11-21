a7e55164610813b8c7d40b3b7fdcf5654463fbe9
neilsen-ngoi <kenisu222@gmail.com>
2023-11-21 19:45:10 +1300

    232

---
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+export type Organization = {
+  id: string
+  slug: string
+  imageUrl: string
+  name: string
+}
+
+interface NavItemProps {
+  isActive: boolean
+  isExpanded: boolean
+  organization: Organization
+  onExpand: (id: string) => void
+}
+
+const NavItem = ({
+  isActive,
+  isExpanded,
+  onExpand,
+  organization,
+}: NavItemProps) => {
+  return <div>NavItem</div>
+}
+
+export default NavItem
