ff2878d6b26cab218b43c16d658d0d3ec1625394
neilsen-ngoi <kenisu222@gmail.com>
2023-11-21 19:08:50 +1300

    accordion skeleton separator

---
+++ app/(platform)/(dashboard)/_components/Sidebar.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import Link from 'next/link'
+import { PlusIcon } from 'lucide-react'
+import { useLocalStorage } from 'usehooks-ts'
+import { useOrganization, useOrganizationList } from '@clerk/nextjs'
+
+import { Button } from '@/components/ui/button'
+import { Separator } from '@/components/ui/separator'
+import { Skeleton } from '@/components/ui/skeleton'
+import { Accordion } from '@/components/ui/accordion'
+
+interface SidebarProps {
+  storageKey?: string
+}
+
+const Sidebar = ({ storageKey }: SidebarProps) => {
+  return <div>Sidebar</div>
+}
+
+export default Sidebar
