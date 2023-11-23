4e7a909ec5a4aaee58bda8befd259c2f81c44a0c
neilsen-ngoi <kenisu222@gmail.com>
2023-11-23 13:13:07 +1300

    sidebar nav item completed

--- app/(platform)/(dashboard)/_components/nav-item.tsx
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -1,12 +1,16 @@
 'use client'
 
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import { usePathname, useRouter } from 'next/navigation'
+import { Activity, CreditCard, Layout, Settings } from 'lucide-react'
+
 import {
-  Accordion,
+  AccordionContent,
   AccordionItem,
   AccordionTrigger,
 } from '@/components/ui/accordion'
-import { cn } from '@/lib/utils'
-import Image from 'next/image'
+import { Button } from '@/components/ui/button'
 
 export type Organization = {
   id: string
@@ -28,6 +32,35 @@ const NavItem = ({
   onExpand,
   organization,
 }: NavItemProps) => {
+  const router = useRouter()
+  const pathname = usePathname()
+  const routes = [
+    {
+      label: 'Boards',
+      icon: <Layout className=" h-4 w-4 mr-2" />,
+      href: `/organization/${organization.id}`,
+    },
+    {
+      label: 'Activity',
+      icon: <Activity className=" h4 w-4 mr-2" />,
+      href: `/organization/${organization.id}/activity`,
+    },
+    {
+      label: 'Settings',
+      icon: <Settings className=" h4 w-4 mr-2" />,
+      href: `/organization/${organization.id}/settings`,
+    },
+    {
+      label: 'Billing',
+      icon: <CreditCard className=" h4 w-4 mr-2" />,
+      href: `/organization/${organization.id}/billing`,
+    },
+  ]
+
+  const onClick = (href: string) => {
+    router.push(href)
+  }
+
   return (
     <AccordionItem value={organization.id} className=" border-none">
       <AccordionTrigger
@@ -37,8 +70,8 @@ const NavItem = ({
           isActive && !isExpanded && 'bg-sky-500/10 text-sky-700'
         )}
       >
-        <div className=" fle items-center gap-x-2 ">
-          <div className=" w-7 h7 relative">
+        <div className=" flex items-center gap-x-2 ">
+          <div className=" w-7 h-7 relative">
             <Image
               fill
               src={organization.imageUrl}
@@ -46,8 +79,26 @@ const NavItem = ({
               className=" rounded-sm object-cover"
             />
           </div>
+          <span className=" font-medium text-sm">{organization.name} </span>
         </div>
       </AccordionTrigger>
+      <AccordionContent className=" pt1 text-neutral-700">
+        {routes.map((route) => (
+          <Button
+            key={route.href}
+            size="sm"
+            onClick={() => onClick(route.href)}
+            className={cn(
+              ' w-full font-normal justify-start pl10 mb-1',
+              pathname === route.href && 'bg-sky-500/10 text-sky-700'
+            )}
+            variant="ghost"
+          >
+            {route.icon}
+            {route.label}
+          </Button>
+        ))}
+      </AccordionContent>
     </AccordionItem>
   )
 }
