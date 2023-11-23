43aefab543a63d7098de5b694f5c788f9b61018c
neilsen-ngoi <kenisu222@gmail.com>
2023-11-23 17:58:40 +1300

    impossible

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import { State, create } from '@/actions/create-board'
+import { Button } from '@/components/ui/button'
+import { useFormState } from 'react-dom'
+
+export const Form = () => {
+  const initialState = { message: null, errors: {} } as State
+  const [state, dispatch] = useFormState(create, initialState)
+  return (
+    <form action={dispatch}>
+      <div>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          required
+          placeholder="Enter board title"
+          className="border-black
+        border p-1"
+        />
+        {state?.errors?.title ? (
+          <div>
+            {state.errors.title?.map((error: string) => (
+              <p key={error} className=" text-rose-500">
+                {error}
+              </p>
+            ))}
+          </div>
+        ) : null}
+      </div>
+      <Button type="submit">Submit</Button>
+    </form>
+  )
+}
--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,24 +1,12 @@
 import { db } from '@/lib/db'
-import { create } from '@/actions/create-board'
-import { Button } from '@/components/ui/button'
 import Board from './board'
+import { Form } from './form'
 
 const OrganizationIdPage = async () => {
   const boards = await db.board.findMany()
   return (
     <div className=" flex flex-col space-y-4">
-      <form action={create}>
-        <input
-          type="text"
-          id="title"
-          name="title"
-          required
-          placeholder="Enter board title"
-          className="border-black
-        border p-1"
-        />
-        <Button type="submit">Submit</Button>
-      </form>
+      <Form />
       <div className=" space-y-2">
         {boards.map((board) => (
           // <div key={board.id}>Board title:{board.title}</div>
