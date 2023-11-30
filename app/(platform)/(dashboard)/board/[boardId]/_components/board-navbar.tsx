7fdb2244be2e857e6ddfd6083e42e09fd96dbf50
neilsen-ngoi <kenisu222@gmail.com>
2023-11-30 17:34:24 +1300

    delete board 6.41

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
@@ -1,5 +1,6 @@
 import { Board } from '@prisma/client'
 import BoardTitleForm from './board-title-form'
+import BoardOptions from './board-options'
 
 interface BoardNavbarProps {
   data: Board
@@ -11,6 +12,9 @@ const BoardNavbar = async ({ data }: BoardNavbarProps) => {
     text-white"
     >
       <BoardTitleForm data={data} />
+      <div className=" ml-auto">
+        <BoardOptions id={data.id} />
+      </div>
     </div>
   )
 }
--- app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form.tsx
@@ -4,13 +4,28 @@ import { ElementRef, useRef, useState } from 'react'
 import { FormInput } from '@/components/form/form-input'
 import { Button } from '@/components/ui/button'
 import { Board } from '@prisma/client'
+import { useAction } from '@/hooks/use-action'
+import { updateBoard } from '@/actions/update-board'
+import { toast } from 'sonner'
 
 interface BoardTitleFormProps {
   data: Board
 }
 const BoardTitleForm = ({ data }: BoardTitleFormProps) => {
+  const { execute } = useAction(updateBoard, {
+    onSuccess: (data) => {
+      toast.success(`Board"${data.title}"updated!`)
+      setTitle(data.title)
+      disableEditing()
+    },
+    onError: (error) => {
+      toast.error(error)
+    },
+  })
   const formRef = useRef<ElementRef<'form'>>(null)
   const inputRef = useRef<ElementRef<'input'>>(null)
+
+  const [title, setTitle] = useState(data.title)
   const [isEditing, setIsEditing] = useState(false)
   const enableEditing = () => {
     setIsEditing(true)
@@ -25,7 +40,14 @@ const BoardTitleForm = ({ data }: BoardTitleFormProps) => {
 
   const onSubmit = (formData: FormData) => {
     const title = formData.get('title') as string
-    console.log('Iam submitted')
+    execute({
+      title,
+      id: data.id,
+    })
+  }
+
+  const onBlur = () => {
+    formRef.current?.requestSubmit()
   }
 
   if (isEditing) {
@@ -38,8 +60,8 @@ const BoardTitleForm = ({ data }: BoardTitleFormProps) => {
         <FormInput
           ref={inputRef}
           id="title"
-          onBlur={() => {}}
-          defaultValue={data.title}
+          onBlur={onBlur}
+          defaultValue={title}
           className=" text-lg font-bold px-[7px] py-1 h-7 bg-transparent focus-visible:outline-none
            focus-visible:ring-transparent border-none"
         />
@@ -53,7 +75,7 @@ const BoardTitleForm = ({ data }: BoardTitleFormProps) => {
       variant="transparent"
       className=" font-bold text-lg h-auto w-auto p-1 px-2"
     >
-      {data.title}
+      {title}
     </Button>
   )
 }
