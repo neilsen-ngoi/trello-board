f945e15b978461a29301f1666517717b6cae208b
neilsen-ngoi <kenisu222@gmail.com>
2023-11-30 14:51:43 +1300

    board list form

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form.tsx
@@ -0,0 +1,16 @@
+'use client'
+import { Button } from '@/components/ui/button'
+import { Board } from '@prisma/client'
+
+interface BoardTitleFormProps {
+  data: Board
+}
+const BoardTitleForm = ({ data }: BoardTitleFormProps) => {
+  return (
+    <Button className=" font-bold text-lg h-auto w-auto p-1 px-2">
+      {data.title}
+    </Button>
+  )
+}
+
+export default BoardTitleForm
--- app/(platform)/(dashboard)/organization/[organizationId]/_components/BoardList.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/BoardList.tsx
@@ -1,8 +1,26 @@
+import { HelpCircle, Key, User2 } from 'lucide-react'
+
+import { Skeleton } from '@/components/ui/skeleton'
 import { Hint } from '@/components/Hints'
 import { FormPopover } from '@/components/form/form-popover'
-import { HelpCircle, User2 } from 'lucide-react'
+import { auth } from '@clerk/nextjs'
+import { db } from '@/lib/db'
+import Link from 'next/link'
 
-export const BoardList = () => {
+export const BoardList = async () => {
+  const { orgId } = auth()
+  if (!orgId) {
+    return '/select-org'
+  }
+
+  const boards = await db.board.findMany({
+    where: {
+      orgId,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  })
   return (
     <div className=" space-y-4">
       <div className=" flex items-center font-semibold text-lg to-neutral-700">
@@ -10,6 +28,17 @@ export const BoardList = () => {
         Your Boards
       </div>
       <div className=" grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+        {boards.map((board) => (
+          <Link
+            key={board.id}
+            style={{ backgroundImage: `url(${board.imageThumbUrl})` }}
+            href={`/board/${board.id}`}
+            className=" group relative aspect-video bg-no-repeat bg-center bg-cover bg-sky-700 rounded-sm h-full w-full p-2 overflow-hidden"
+          >
+            <div className=" absolute inset-0 bg-black/30 group-hover:bg-black/40 transition" />
+            <p className="relative font-semibold text-white">{board.title}</p>
+          </Link>
+        ))}
         <FormPopover sideOffset={10} side="right">
           <div
             role="button"
@@ -27,6 +56,21 @@ export const BoardList = () => {
           </div>
         </FormPopover>
       </div>
+    </div>
+  )
+}
+
+BoardList.Skeleton = function SkeletonBoardList() {
+  return (
+    <div className=" grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+      <Skeleton className=" aspect-video h-full w-full p-2" />
+      <Skeleton className=" aspect-video h-full w-full p-2" />
+      <Skeleton className=" aspect-video h-full w-full p-2" />
+      <Skeleton className=" aspect-video h-full w-full p-2" />
+      <Skeleton className=" aspect-video h-full w-full p-2" />
+      <Skeleton className=" aspect-video h-full w-full p-2" />
+      <Skeleton className=" aspect-video h-full w-full p-2" />
+      <Skeleton className=" aspect-video h-full w-full p-2" />
     </div>
   )
 }
