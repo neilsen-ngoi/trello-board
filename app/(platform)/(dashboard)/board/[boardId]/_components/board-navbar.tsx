078532d5771d7ad69cd3210631a4bac95a3485c0
neilsen-ngoi <kenisu222@gmail.com>
2023-11-30 16:38:03 +1300

    board title form editing

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
@@ -7,7 +7,7 @@ interface BoardNavbarProps {
 const BoardNavbar = async ({ data }: BoardNavbarProps) => {
   return (
     <div
-      className=" w-full h-14 z-[40] bg-black/50 fixed top 14 flex items-center px-6 gap-x-4
+      className=" w-full h-14 z-[40] bg-black/50 fixed top-14 flex items-center px-6 gap-x-4
     text-white"
     >
       <BoardTitleForm data={data} />
