2cc0e202e3eb6943915a61019913ff7325205b83
neilsen-ngoi <kenisu222@gmail.com>
2023-12-01 16:02:07 +1300

    editing the list in boards

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -1,6 +1,8 @@
 'use client'
 import { ListWithCards } from '@/types'
 import ListForm from './list-form'
+import { useEffect, useState } from 'react'
+import ListItem from './list-item'
 
 interface ListContainerProps {
   data: ListWithCards[]
@@ -8,8 +10,18 @@ interface ListContainerProps {
 }
 
 const ListContainer = ({ data, boardId }: ListContainerProps) => {
+  const [orderedData, setOrderedData] = useState(data)
+  // source of truth to modify in the front before sending to the db
+
+  useEffect(() => {
+    setOrderedData(data)
+  }, [data])
+
   return (
-    <ol>
+    <ol className=" flex gap-x-3 h-full">
+      {orderedData.map((data, index) => {
+        return <ListItem key={data.id} index={index} data={data} />
+      })}
       <ListForm />
       <div className=" flex-shrink-0 w-1" />
     </ol>
--- app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
@@ -3,14 +3,18 @@ import ListWrapper from './list-wrapper'
 import { ElementRef, useRef, useState } from 'react'
 import { useEventListener, useOnClickOutside } from 'usehooks-ts'
 import { FormInput } from '@/components/form/form-input'
-import { useParams } from 'next/navigation'
+import { useParams, useRouter } from 'next/navigation'
 import { FormSubmit } from '@/components/form/form-submit'
 import { Button } from '@/components/ui/button'
+import { useAction } from '@/hooks/use-action'
+import { createList } from '@/actions/create-list'
+import { toast } from 'sonner'
+import { error } from 'console'
 
 const ListForm = () => {
   const formRef = useRef<ElementRef<'form'>>(null)
   const inputRef = useRef<ElementRef<'input'>>(null)
-
+  const router = useRouter()
   const [isEditing, setIsEditing] = useState(false)
   const params = useParams()
   const enableEditing = () => {
@@ -24,6 +28,17 @@ const ListForm = () => {
     setIsEditing(false)
   }
 
+  const { execute, fieldErrors } = useAction(createList, {
+    onSuccess: (data) => {
+      toast.success(`List"${data.title}" created`)
+      disableEditing()
+      router.refresh()
+    },
+    onError: (error) => {
+      toast.error(error)
+    },
+  })
+
   const onKeyDown = (e: KeyboardEvent) => {
     if (e.key === 'Escape') {
       disableEditing()
@@ -32,16 +47,23 @@ const ListForm = () => {
 
   useEventListener('keydown', onKeyDown)
   useOnClickOutside(formRef, disableEditing)
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get('title') as string
+    const boardId = formData.get('boardId') as string
+    execute({ title, boardId })
+  }
 
   if (isEditing) {
     return (
       <ListWrapper>
         <form
           ref={formRef}
+          action={onSubmit}
           className=" w-full p-3 rounded-md bg-white space-y-4 shadow-md"
         >
           <FormInput
             ref={inputRef}
+            errors={fieldErrors}
             id="title"
             className=" text-sm hover:bsorder-input focus:border-input transition px-2 py-1 font-medium border-transparent"
             placeholder="Enter list title"
