10f0e8c13ed22af7f58a96e84bbd648138146bcb
neil <kenisu222@gmail.com>
2023-12-07 15:45:57 +1300

    add modal to card

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -2,11 +2,14 @@
 
 import { Card } from '@prisma/client'
 import { Draggable } from '@hello-pangea/dnd'
+import { useCardModal } from '@/hooks/use-card-modal'
 interface CardItemProps {
   index: number
   data: Card
 }
 const CardItem = ({ data, index }: CardItemProps) => {
+  const cardModal = useCardModal()
+
   return (
     <Draggable draggableId={data.id} index={index}>
       {(provided) => (
@@ -16,6 +19,7 @@ const CardItem = ({ data, index }: CardItemProps) => {
           ref={provided.innerRef}
           role="button"
           className=" truncate border-2 border-transparent hover:border-black py-2 px-3 bg-white rounded-md shadow-sm"
+          onClick={() => cardModal.onOpen(data.id)}
         >
           {data.title}
         </div>
