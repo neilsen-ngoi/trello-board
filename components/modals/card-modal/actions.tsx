d5c0ec91c6b0d4ff8e44d3224bc84d313f527d94
neil <kenisu222@gmail.com>
2023-12-08 12:55:53 +1300

    actions for description edit

---
+++ components/modals/card-modal/actions.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import { Skeleton } from '@/components/ui/skeleton'
+import { CardWithList } from '@/types'
+import { Copy } from 'lucide-react'
+
+interface ActionsProps {
+  data: CardWithList
+}
+
+export const Actions = ({ data }: ActionsProps) => {
+  return (
+    <div className=" space-y-2 mt-2">
+      <p className=" text-xs font-semibold">Actions</p>
+      <Button variant="gray" className=" w-full justify-start" size="inline">
+        <Copy className=" h-4 w-4 mr-2" />
+        Copy
+      </Button>
+    </div>
+  )
+}
+
+Actions.Skeleton = function ActionsSkeleton() {
+  return (
+    <div className="  mt-2 space-y-2">
+      <Skeleton className=" w-20 h-4 bg-neutral-200" />
+      <Skeleton className=" w-full h-8 bg-neutral-200" />
+      <Skeleton className=" w-full h-8 bg-neutral-200" />
+    </div>
+  )
+}
--- components/modals/card-modal/description.tsx
+++ components/modals/card-modal/description.tsx
@@ -1,14 +1,18 @@
 'use client'
 
+import { updateCard } from '@/actions/update-card'
+import { FormSubmit } from '@/components/form/form-submit'
+import { FormTextarea } from '@/components/form/form-textarea'
+import { Button } from '@/components/ui/button'
 import { Skeleton } from '@/components/ui/skeleton'
+import { useAction } from '@/hooks/use-action'
 import { CardWithList } from '@/types'
 import { useQueryClient } from '@tanstack/react-query'
 import { AlignLeft } from 'lucide-react'
 import { useParams } from 'next/navigation'
-import { off } from 'process'
-import { ElementRef, KeyboardEvent, useRef, useState } from 'react'
+import { ElementRef, useRef, useState } from 'react'
+import { toast } from 'sonner'
 import { useEventListener, useOnClickOutside } from 'usehooks-ts'
-
 interface DescriptionProps {
   data: CardWithList
 }
@@ -40,19 +44,63 @@ export const Description = ({ data }: DescriptionProps) => {
   useEventListener('keydown', onKeyDown)
   useOnClickOutside(formRef, disableEditing)
 
+  const { execute, fieldErrors } = useAction(updateCard, {
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: ['card', data.id] })
+      toast.success(`Card"${data.title}" updated`)
+      disableEditing()
+    },
+    onError: (error) => {
+      toast.error(error)
+    },
+  })
+
+  const onSubmit = (formData: FormData) => {
+    const description = formData.get('description') as string
+    const boardId = params.boardId as string
+
+    execute({ id: data.id, description, boardId })
+  }
+
+  //use updatecard server action for description editing
+
   return (
     <div className=" flex items-start gap-x-3 w-full">
       <AlignLeft className=" h-5 w-5 mt-0.5 text-neutral-700" />
       <div className=" w-full">
         <p className=" font-semibold text-neutral-700 mb-2">Description</p>
+        {isEditing ? (
+          <form action={onSubmit} ref={formRef} className=" space-y-2">
+            <FormTextarea
+              ref={textareaRef}
+              id="description"
+              className=" w-full mt-2"
+              placeholder=" add more detailed description"
+              defaultValue={data.description || undefined}
+              errors={fieldErrors}
+            />
+            <div className=" flex item-center gap-x-2">
+              <FormSubmit>Save</FormSubmit>
+              <Button
+                type="button"
+                onClick={disableEditing}
+                size="sm"
+                variant="ghost"
+              >
+                Cancal
+              </Button>
+            </div>
+          </form>
+        ) : (
+          <div
+            onClick={enableEditing}
+            role="button"
+            className=" min-h-[78px] px-3.5 bg-neutral-200 text-sm font-medium py-3  rounded-md"
+          >
+            {data.description || 'add a more detailed description'}
+          </div>
+        )}
       </div>
-      <div
-        role="button"
-        className=" min-h-[78px] px-3.5 bg-neutral-200 text-sm font-medium py-3  rounded-md"
-      >
-        {data.description || 'add a more detailed description'}
-      </div>
-      {data.description}
     </div>
   )
 }
